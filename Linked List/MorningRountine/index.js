0603f709a3eac62996d2c36f62391d725173c2e7
SamaraSilvia21 <samara.silvia@bidweb.com.br>
2022-09-22 14:54:24 -0300

    update files linked list

--- Linked List/MorningRountine/index.js
+++ Linked List/MorningRountine/index.js
@@ -4,13 +4,18 @@ let head;
 let previous;
 let data;
 let next;
+let out;
+let contador = 0;
+
 let inputRoutine = document.querySelector('#inputRoutine').value;
+
 let contents;
 let node_data;
 let current_node;
 let previous_node;
 
 class Node { // cada nó será composto por esss elementos
+
     constructor (value){
         this.data = value;
         this.next = null;
@@ -28,7 +33,6 @@ class MorningRoutine {
     addRoutine(inputRoutine){
 
         const new_node = new Node(inputRoutine); // chama o construtor
-
         new_node.next = null
     
         if(this.tail){
@@ -45,21 +49,87 @@ class MorningRoutine {
     display() { // mostra os elementos da lista encadeada
 
         contents = this.head // conteúdo da função
-    
+
         if(contents == null){
             console.log("List has no element")
         }
 
         while(contents){
             console.log(contents.data)
+            document.getElementById('div').innerHTML += `<p>${contents.data}</p>`
             contents = contents.next
+        }
+    }
+
+    removeAll(){
+
+        if(this.head === null){
+            return null;
+        } else {
+
+            out = this.head
+            this.head = this.head.previous_node  // como não é circular, não tem elemento antes dele.
+            console.log("All elements was removed")
+
+            if(contador > 0){
+                contador--
+            }
+
+            return out.data
         }
     }
 }
 
 const myList = new MorningRoutine();
-myList.display();
 
+// Modificações no HTML
 
+function adicionarElementos() { // mostra os elementos da lista encadeada
 
+    document.getElementById('div').innerHTML = ""
 
+    let inputRoutine = document.querySelector('#inputRoutine').value;
+
+    if(inputRoutine == ""){
+        alert("Você precisa digitar algo!!")
+        console.log("Digite algo")
+    } else{
+        console.log(inputRoutine)
+        myList.addRoutine(inputRoutine)
+        console.log(myList.display())
+    }
+}
+
+let div = document.getElementById('div');
+let btnRemover = document.querySelector('#btnRemover');
+
+btnRemover.onclick = (evt) => removerElementos(evt,div)
+
+function removerElementos(evt,div) {
+
+    if(evt.target.onclick){
+        div = myList.removeAll();
+    }   
+}
+
+myList.addRoutine(1)
+myList.addRoutine(2)
+myList.addRoutine(3)
+myList.display()
+myList.removeAll()
+myList.display()
+
+/* 
+
+ let btnRemover = document.querySelector('#btnRemover').value;
+
+        if(btnRemover){
+            document.getElementById('div').innerHTML = null
+            console.log(myList.removeAll())
+        } else{
+            console.log("Não há elementos para deletar")
+        }
+*/
+
+// Eu quero deletar cada elemento;
+// Eu quero que após apertar o botão o valor se apague no input - limpesa automática;
